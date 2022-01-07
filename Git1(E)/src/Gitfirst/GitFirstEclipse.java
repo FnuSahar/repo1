7054fec68ceb85d355739dc0b116eaaa2e587b3f
FnuSahar <atharsahar@yahoo.com>
2022-01-07 17:13:39 -0500

    this is my commit

---
+++ Git1(E)/src/Gitfirst/GitFirstEclipse.java
@@ -0,0 +1,14 @@
+package Gitfirst;
+
+public class GitFirstEclipse {
+
+	public static void main(String[] args) {
+		
+		String x= "Alex";
+		String y="David";
+		
+		System.out.println(x+y);
+
+	}
+
+}
